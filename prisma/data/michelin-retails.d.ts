6e9bf791e67ebf6bec29ef796f6a895e6971ecde
Sahrane_Guassemi <sahranedev@gmail.com>
2026-06-16 16:33:15 +0200

    seeding retail

---
+++ prisma/data/michelin-retails.d.ts
@@ -0,0 +1,9 @@
+export type RetailSeed = {
+    name: string;
+    address: string;
+    longitude: number;
+    latitude: number;
+    phoneNumber: string;
+    websiteUrl: string;
+};
+export declare const michelinRetails: RetailSeed[];
