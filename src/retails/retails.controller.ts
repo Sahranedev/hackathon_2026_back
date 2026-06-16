1d66efcf141e4691b0a341d36fa3aaf085320ea8
Sahrane_Guassemi <sahranedev@gmail.com>
2026-06-16 16:53:53 +0200

    ajout role guard sur la route des retails

--- src/retails/retails.controller.ts
+++ src/retails/retails.controller.ts
@@ -14,30 +14,32 @@ import { UpdateRetailDto } from './dto/update-retail.dto';
 import { RetailsService } from './retails.service';
 import { Roles } from '../security/decorators/roles.decorator';
 import { Role } from '../users/enums/role.enum';
-import { JwtAuthGuard } from 'src/security/guards/jwt-auth.guard';
-import { RolesGuard } from 'src/security/guards/roles.guard';
+import { RolesGuard } from '../security/guards/roles.guard';
 
-@UseGuards(RolesGuard, JwtAuthGuard)
-@Roles(Role.Admin)
+@UseGuards(RolesGuard)
 @Controller('api/retails')
 export class RetailsController {
   constructor(private readonly retailsService: RetailsService) {}
 
+  @Roles(Role.Admin)
   @Post()
   create(@Body() createRetailDto: CreateRetailDto) {
     return this.retailsService.create(createRetailDto);
   }
 
+  @Roles(Role.User, Role.Admin)
   @Get()
   findAll() {
     return this.retailsService.findAll();
   }
 
+  @Roles(Role.User, Role.Admin)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.retailsService.findOne(id);
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -46,6 +48,7 @@ export class RetailsController {
     return this.retailsService.update(id, updateRetailDto);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.retailsService.remove(id);
