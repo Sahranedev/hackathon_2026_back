a7262dfd16a59f85e589780340f4889ca3a5400d
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2026-06-18 08:47:31 +0200

    Fix build

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -6,8 +6,9 @@ import {
   Param,
   ParseIntPipe,
   Post,
-  Req,
 } from '@nestjs/common';
+import type { AuthenticatedUser } from 'src/auth/types/authenticated-request.type';
+import { CurrentUser } from 'src/security/decorators/current-user.decorator';
 import { ActivitiesService } from './activities.service';
 import { AddGpsPointDto } from './dto/add-gps-point.dto';
 import { FinishActivityDto } from './dto/finish-activity.dto';
@@ -18,29 +19,19 @@ export class ActivitiesController {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
   @Get()
-  async findAll(@Req() req: any) {
-    const userId = req.user.id;
-
-    return this.activitiesService.findAll(userId);
-  }
-
-  @Get('terrain-types')
-  getTerrainTypes() {
-    return this.activitiesService.getTerrainTypes();
-  }
-
-  @Get(':id')
-  async findOne(@Req() req: any, @Param('id', ParseIntPipe) id: number) {
-    const userId = req.user.id;
-
-    return this.activitiesService.findOne(userId, id);
+  async findAll(@CurrentUser() authenticatedUser: AuthenticatedUser) {
+    return this.activitiesService.findAll(authenticatedUser.id);
   }
 
   @Post('start')
-  async start(@Req() req: any, @Body() startActivityDto: StartActivityDto) {
-    const userId = req.user.id;
-
-    return this.activitiesService.startActivity(userId, startActivityDto);
+  async start(
+    @CurrentUser() authenticatedUser: AuthenticatedUser,
+    @Body() startActivityDto: StartActivityDto,
+  ) {
+    return this.activitiesService.startActivity(
+      authenticatedUser.id,
+      startActivityDto,
+    );
   }
 
   @Get('terrain-types')
@@ -49,38 +40,44 @@ export class ActivitiesController {
   }
 
   @Get(':id')
-  async findOne(@Req() req: any, @Param('id', ParseIntPipe) id: number) {
-    const userId = req.user.id;
-
-    return this.activitiesService.findOne(userId, id);
+  async findOne(
+    @CurrentUser() authenticatedUser: AuthenticatedUser,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.activitiesService.findOne(authenticatedUser.id, id);
   }
 
   @Post(':id/points')
   async addGpsPoint(
-    @Req() req: any,
+    @CurrentUser() authenticatedUser: AuthenticatedUser,
     @Param('id', ParseIntPipe) id: number,
     @Body() addGpsPointDto: AddGpsPointDto,
   ) {
-    const userId = req.user.id;
-
-    return this.activitiesService.addGpsPoint(userId, id, addGpsPointDto);
+    return this.activitiesService.addGpsPoint(
+      authenticatedUser.id,
+      id,
+      addGpsPointDto,
+    );
   }
 
   @Post(':id/finish')
   async finish(
-    @Req() req: any,
+    @CurrentUser() authenticatedUser: AuthenticatedUser,
     @Param('id', ParseIntPipe) id: number,
     @Body() finishActivityDto: FinishActivityDto,
   ) {
-    const userId = req.user.id;
-
-    return this.activitiesService.finishActivity(userId, id, finishActivityDto);
+    return this.activitiesService.finishActivity(
+      authenticatedUser.id,
+      id,
+      finishActivityDto,
+    );
   }
 
   @Delete(':id')
-  async delete(@Req() req: any, @Param('id', ParseIntPipe) id: number) {
-    const userId = req.user.id;
-
-    return this.activitiesService.deleteActivity(userId, id);
+  async delete(
+    @CurrentUser() authenticatedUser: AuthenticatedUser,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.activitiesService.deleteActivity(authenticatedUser.id, id);
   }
 }
