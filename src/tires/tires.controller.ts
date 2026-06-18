57c0019a09c342b885e1474e125e3afd8ce4247e
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2026-06-18 16:07:45 +0200

    Implement route to search and add tire

--- src/tires/tires.controller.ts
+++ src/tires/tires.controller.ts
@@ -1,13 +1,18 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import type { AuthenticatedUser } from 'src/auth/types/authenticated-request.type';
 import { CurrentUser } from 'src/security/decorators/current-user.decorator';
 import { JwtAuthGuard } from 'src/security/guards/jwt-auth.guard';
+import { CreateUserTireDto } from './dto/create-user-tire.dto';
 import { TiresService } from './tires.service';
 
 @UseGuards(JwtAuthGuard)
@@ -20,12 +25,36 @@ export class TiresController {
     return this.tiresService.getUserTires(authenticatedUser.id);
   }
 
+  @Post('mine')
+  addUserTire(
+    @CurrentUser() authenticatedUser: AuthenticatedUser,
+    @Body() createUserTireDto: CreateUserTireDto,
+  ) {
+    return this.tiresService.addUserTire(
+      authenticatedUser.id,
+      createUserTireDto.tireId,
+    );
+  }
+
   @Get('mine/:id/info')
   getUserTireInfo(
     @CurrentUser() authenticatedUser: AuthenticatedUser,
     @Param('id', ParseIntPipe) id: number,
   ) {
     return this.tiresService.getUserTireInfo(authenticatedUser.id, id);
+  }
+
+  @Delete('mine/:id')
+  deleteUserTire(
+    @CurrentUser() authenticatedUser: AuthenticatedUser,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.tiresService.deleteUserTire(authenticatedUser.id, id);
+  }
+
+  @Get('catalog/search')
+  searchTireCatalog(@Query('q') query = '') {
+    return this.tiresService.searchTireCatalog(query);
   }
 
   @Get('/model/:id')
